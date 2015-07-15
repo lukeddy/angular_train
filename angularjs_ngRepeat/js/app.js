848b7b6b8025eff05e4f6780d87b297cf9b66779
tzq668766 <tzq618766@gmail.com>
2015-07-15 10:24:12 +0800

    添加ngRepeat迭代输出指令示例

---
+++ angularjs_ngRepeat/js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 15-3-5.
+ */
+var myApp = angular.module('myApp', []); //注意app创建时需要添加第二个参数
+
+myApp.controller('MainController',function($scope,$timeout){
+    //TODO logic
+});
