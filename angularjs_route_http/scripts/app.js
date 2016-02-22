3ec73c00661a3ff553029e22d350d7f81e27b6c7
tzq668766 <tzq618766@gmail.com>
2016-02-22 16:15:18 +0800

    在$rootScope中添加全局数据，使得每个页面都可以访问

--- angularjs_route_http/scripts/app.js
+++ angularjs_route_http/scripts/app.js
@@ -1,4 +1,8 @@
 var routeApp = angular.module('movieApp',['ngRoute']);
+
+routeApp.run(function($rootScope) {
+    $rootScope.appname = "欢迎使用AngularJS";
+});
 routeApp.config(['$routeProvider',function ($routeProvider) {
     $routeProvider.when('/movieList',{
             templateUrl:'views/route/movie_list.html',
