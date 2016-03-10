f442aa173979cb695f784688bde13ed319972ed0
tzq668766 <tzq618766@gmail.com>
2016-03-10 18:12:11 +0800

    添加angular表单验证 添加angular 增删改示例，目前添加数据时还有问题。

---
+++ angularjs_crud/assets/js/app.js
@@ -0,0 +1,64 @@
+angular.module('CrudApp', [ 'ngRoute']).config(['$routeProvider', function($routeProvider) {
+      $routeProvider.when('/', {templateUrl: 'assets/tpl/lists.html', controller: ListCtrl}).
+      when('/add-user', {templateUrl: 'assets/tpl/add-new.html', controller: AddCtrl}).
+      when('/edit/:id', {templateUrl: 'assets/tpl/edit.html', controller: EditCtrl}).
+      otherwise({redirectTo: '/'});
+}]);
+
+
+function ListCtrl($scope, $http) {
+  $http.get('assets/data/users.json').success(function(data) {
+    console.log(data);
+    $scope.users = data.users;
+  });
+}
+
+function AddCtrl($scope, $http, $location) {
+  $scope.master = {};
+  $scope.activePath = null;
+
+  $scope.add_new = function(user, AddNewForm) {
+    //$http.post('api/add_user', user).success(function(){
+    //  $scope.reset();
+    //  $scope.activePath = $location.path('/');
+    //});
+    console.log(user);
+    $scope.users.push(user);
+    console.log($scope.users);
+    //$scope.activePath = $location.path('/');
+    $scope.reset();
+
+    $scope.reset = function() {
+      $scope.user = angular.copy($scope.master);
+    };
+
+    $scope.reset();
+
+  };
+}
+
+function EditCtrl($scope, $http, $location, $routeParams) {
+  var id = $routeParams.id;
+  $scope.activePath = null;
+
+  $http.get('api/users/'+id).success(function(data) {
+    $scope.users = data;
+  });
+
+  $scope.update = function(user){
+    $http.put('api/users/'+id, user).success(function(data) {
+      $scope.users = data;
+      $scope.activePath = $location.path('/');
+    });
+  };
+
+  $scope.delete = function(user) {
+    console.log(user);
+
+    var deleteUser = confirm('Are you absolutely sure you want to delete?');
+    if (deleteUser) {
+      $http.delete('api/users/'+user.id);
+      $scope.activePath = $location.path('/');
+    }
+  };
+}
